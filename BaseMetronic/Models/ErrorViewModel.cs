4137e26755fab745dd3f5a116972343b738771bd
Tùng Tạ Duy <taduytung24112000@gmail.com>
2024-09-20 10:19:43 +0700

    Add project files.

---
+++ BaseMetronic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaseMetronic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
